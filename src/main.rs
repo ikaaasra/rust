5e1ae2cc4d2665975218919ed7c47f26fdd342cb
Kasra Alizadeh <ikaaasra@gmail.com>
2023-05-24 13:03:03 +0330

    update main.rs
    add axum config and health_handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello World!");
+use axum::{response::IntoResponse, routing::get, Json, Router};
+
+async fn health_handler() -> impl IntoResponse {
+    return Json(
+        serde_json::json!({"status":"success","message":"Build Simple CRUD(CREATE,READ,UPDATE,DELETE) API in Rust using Axum"}),
+    );
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/api/health", get(health_handler));
+
+    println!("🚀 Server started successfully");
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
