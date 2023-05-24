5050d5fd9d1a4d007d9d4c58d66002a35d6756e3
Kasra Alizadeh <ikaaasra@gmail.com>
2023-05-24 13:53:29 +0330

    update main.ts
    remove health handler and add new router

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
-use axum::{response::IntoResponse, routing::get, Json, Router};
-
-async fn health_handler() -> impl IntoResponse {
-    return Json(
-        serde_json::json!({"status":"success","message":"Build Simple CRUD(CREATE,READ,UPDATE,DELETE) API in Rust using Axum"}),
-    );
-}
+mod handler;
+mod model;
+mod response;
+mod route;
+use route::router;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/api/health", get(health_handler));
+    let app = router();
 
     println!("🚀 Server started successfully");
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
