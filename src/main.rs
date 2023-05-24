c694508eb822b58cd0758c9bc322652dbb717967
Kasra Alizadeh <ikaaasra@gmail.com>
2023-05-24 13:56:53 +0330

    update main.rs
    add CORS

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
+use axum::http::{
+    header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
+    HeaderValue, Method,
+};
+use route::router;
+use tower_http::cors::CorsLayer;
+
 mod handler;
 mod model;
 mod response;
 mod route;
-use route::router;
 
 #[tokio::main]
 async fn main() {
-    let app = router();
+    let cors = CorsLayer::new()
+        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
+        .allow_credentials(true)
+        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
+
+    let app = router().layer(cors);
 
     println!("🚀 Server started successfully");
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
