682dff2ae0907b976a331328b36cdf51c15e1109
Kasra Alizadeh <ikaaasra@gmail.com>
2023-05-24 14:11:20 +0330

    add get_get_handler

--- src/handler.rs
+++ src/handler.rs
@@ -16,8 +16,33 @@ pub async fn health_handler() -> impl IntoResponse {
     );
 }
 
+// ----------------------------------------------------------------- CREATE_TODO
 // pub async fn create_todo_handler()-> {}
-// pub async fn get_todo_handler()-> {}
+
+// ----------------------------------------------------------------- GET_TODO
+pub async fn get_todo_handler(
+    Path(id): Path<Uuid>,
+    State(db): State<DB>,
+) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
+    let id = id.to_string();
+    let vec = db.lock().await;
+
+    if let Some(todo) = vec.iter().find(|todo| todo.id == Some(id.to_owned())) {
+        let json_response = ToDoSingleResponse {
+            status: "success".to_string(),
+            data: todo.clone(),
+        };
+        return Ok((StatusCode::OK, Json(json_response)));
+    }
+
+    let error_response = serde_json::json!({
+        "status": "fail",
+        "message": format!("Todo with ID: {} not found", id)
+    });
+    Err((StatusCode::NOT_FOUND, Json(error_response)))
+}
+
+// ----------------------------------------------------------------- GET_TODOS
 pub async fn get_todos_handler(
     options: Option<Query<QueryOptions>>,
     State(db): State<DB>,
@@ -35,5 +60,9 @@ pub async fn get_todos_handler(
         data: todos,
     })
 }
+
+// ----------------------------------------------------------------- UPDATE_TODO
 // pub async fn update_todo_handler()-> {}
+
+// ----------------------------------------------------------------- DELETE_TODO
 // pub async fn delete_todo_handler()-> {}
--- src/route.rs
+++ src/route.rs
@@ -1,5 +1,5 @@
 use crate::{
-    handler::{get_todos_handler, health_handler},
+    handler::{get_todo_handler, get_todos_handler, health_handler},
     model,
 };
 use axum::{routing::get, Router};
@@ -8,7 +8,8 @@ pub fn router() -> Router {
     let db = model::todo_db();
 
     return Router::new()
-        .route("/api/todos", get(get_todos_handler))
         .route("/api/health", get(health_handler))
+        .route("/api/todos", get(get_todos_handler))
+        .route("/api/todos/:id", get(get_todo_handler))
         .with_state(db);
 }
