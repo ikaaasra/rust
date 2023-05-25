6fc162166b424136b5c2a795cf2a6148a1a29ffb
Kasra Alizadeh <ikaaasra@gmail.com>
2023-05-26 02:44:27 +0330

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ use axum::http::{
     header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
     HeaderValue, Method,
 };
+use config::Config;
 use dotenv::dotenv;
 use route::router;
 use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 use std::sync::Arc;
 use tower_http::cors::CorsLayer;
 
+mod config;
 mod handler;
 mod model;
 mod route;
@@ -15,17 +17,18 @@ mod schema;
 
 pub struct AppState {
     db: Pool<Postgres>,
+    env: Config,
 }
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
 
-    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let config = Config::init();
 
     let pool = match PgPoolOptions::new()
         .max_connections(10)
-        .connect(&database_url)
+        .connect(&config.database_url)
         .await
     {
         Ok(pool) => {
@@ -45,7 +48,11 @@ async fn main() {
         .allow_credentials(true)
         .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
 
-    let app = router(Arc::new(AppState { db: pool.clone() })).layer(cors);
+    let app = router(Arc::new(AppState {
+        db: pool.clone(),
+        env: config.clone(),
+    }))
+    .layer(cors);
 
     println!("🚀 Server started successfully");
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
