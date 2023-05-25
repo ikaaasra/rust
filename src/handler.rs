d8fd395e4fe522ead701a7edfeffab8ad023ed15
Kasra Alizadeh <ikaaasra@gmail.com>
2023-05-26 01:29:54 +0330

    update handlers

--- src/handler.rs
+++ src/handler.rs
@@ -1,6 +1,10 @@
 use crate::{
-    model::{QueryOptions, ToDo, UpdateToDo, DB},
-    response::{ToDoListResponse, ToDoSingleResponse},
+    model::ToDoModel,
+    schema::{
+        CreateToDo, FilterOptions, GenericResponse, ToDoListResponse, ToDoSingleResponse,
+        UpdateToDo,
+    },
+    AppState,
 };
 use axum::{
     extract::{Path, Query, State},
@@ -8,159 +12,200 @@ use axum::{
     response::IntoResponse,
     Json,
 };
-use uuid::Uuid;
+use serde_json::json;
+use std::sync::Arc;
 
 pub async fn health_handler() -> impl IntoResponse {
-    return Json(
-        serde_json::json!({"status":"success","message":"Build Simple CRUD(CREATE,READ,UPDATE,DELETE) API in Rust using Axum"}),
-    );
+    let json_response = serde_json::json!(GenericResponse {
+        status: "success".to_string(),
+        message: "Build Simple CRUD(CREATE,READ,UPDATE,DELETE) API in Rust using Axum".to_string(),
+    });
+
+    return Json(json_response);
 }
 
 // ----------------------------------------------------------------- CREATE_TODO
 pub async fn create_todo_handler(
-    State(db): State<DB>,
-    Json(mut body): Json<ToDo>,
+    State(data): State<Arc<AppState>>,
+    Json(body): Json<CreateToDo>,
 ) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
-    let mut vec = db.lock().await;
+    let query = sqlx::query_as!(
+        ToDoModel,
+        "INSERT INTO todo (title,content) VALUES ($1,$2) RETURNING *",
+        body.title.to_string(),
+        body.content.to_string(),
+    )
+    .fetch_one(&data.db)
+    .await;
 
-    if let Some(todo) = vec.iter().find(|todo| todo.title == body.title) {
-        let error_response = serde_json::json!({
-            "status":"fail",
-            "message":format!("Todo with title: '{}' already exists", todo.title),
-        });
-        return Err((StatusCode::CONFLICT, Json(error_response)));
+    match query {
+        Ok(todo) => {
+            let json_response = serde_json::json!(ToDoSingleResponse {
+                status: "".to_string(),
+                data: todo,
+            });
+            return Ok((StatusCode::CREATED, Json(json_response)));
+        }
+        Err(err) => {
+            if err
+                .to_string()
+                .contains("duplicate key value violates unique constraint")
+            {
+                let error_response = serde_json::json!(GenericResponse {
+                    status: "fail".to_string(),
+                    message: "ToDo with that title already exists".to_string(),
+                });
+                return Err((StatusCode::INTERNAL_SERVER_ERROR, Json(error_response)));
+            }
+
+            let error_response = serde_json::json!(GenericResponse {
+                status: "error".to_string(),
+                message: format!("{:?}", err),
+            });
+            return Err((StatusCode::INTERNAL_SERVER_ERROR, Json(error_response)));
+        }
     }
-
-    let uuid_id = Uuid::new_v4();
-    let datetime = chrono::Utc::now();
-
-    body.id = Some(uuid_id.to_string());
-    body.complete = Some(false);
-    body.createdAt = Some(datetime);
-    body.updatedAt = Some(datetime);
-
-    let todo = body.to_owned();
-    vec.push(body);
-
-    let json_response = ToDoSingleResponse {
-        status: "success".to_string(),
-        data: todo,
-    };
-
-    return Ok((StatusCode::CREATED, Json(json_response)));
 }
 
 // ----------------------------------------------------------------- GET_TODO
 pub async fn get_todo_handler(
-    Path(id): Path<Uuid>,
-    State(db): State<DB>,
+    Path(id): Path<uuid::Uuid>,
+    State(data): State<Arc<AppState>>,
 ) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
-    let id = id.to_string();
-    let vec = db.lock().await;
+    let query = sqlx::query_as!(ToDoModel, "SELECT * FROM todo WHERE id = $1", id)
+        .fetch_one(&data.db)
+        .await;
 
-    if let Some(todo) = vec.iter().find(|todo| todo.id == Some(id.to_owned())) {
-        let json_response = ToDoSingleResponse {
-            status: "success".to_string(),
-            data: todo.clone(),
-        };
-        return Ok((StatusCode::OK, Json(json_response)));
+    match query {
+        Ok(todo) => {
+            let json_response = serde_json::json!(ToDoSingleResponse {
+                status: "".to_string(),
+                data: todo,
+            });
+            return Ok((StatusCode::CREATED, Json(json_response)));
+        }
+        Err(_) => {
+            let error_response = serde_json::json!(GenericResponse {
+                status: "".to_string(),
+                message: format!("ToDo with ID: {} not found", id)
+            });
+
+            return Err((StatusCode::NOT_FOUND, Json(error_response)));
+        }
     }
-
-    let error_response = serde_json::json!({
-        "status": "fail",
-        "message": format!("Todo with ID: {} not found", id)
-    });
-    return Err((StatusCode::NOT_FOUND, Json(error_response)));
 }
 
 // ----------------------------------------------------------------- GET_TODOS
 pub async fn get_todos_handler(
-    options: Option<Query<QueryOptions>>,
-    State(db): State<DB>,
-) -> impl IntoResponse {
-    let todos = db.lock().await;
+    options: Option<Query<FilterOptions>>,
+    State(data): State<Arc<AppState>>,
+) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
     let Query(options) = options.unwrap_or_default();
+
     let limit = options.limit.unwrap_or(10);
     let offset = (options.page.unwrap_or(1) - 1) * limit;
 
-    let todos: Vec<ToDo> = todos.clone().into_iter().skip(offset).take(limit).collect();
+    let query = sqlx::query_as!(
+        ToDoModel,
+        "SELECT * FROM todo ORDER by id LIMIT $1 OFFSET $2",
+        limit as i32,
+        offset as i32
+    )
+    .fetch_all(&data.db)
+    .await;
 
-    Json(ToDoListResponse {
+    if query.is_err() {
+        let error_response = serde_json::json!(GenericResponse {
+            status: "fail".to_string(),
+            message: "Something bad happened while fetching all todo items".to_string(),
+        });
+        return Err((StatusCode::INTERNAL_SERVER_ERROR, Json(error_response)));
+    }
+
+    let todos = query.unwrap();
+
+    let json_response = serde_json::json!(ToDoListResponse {
         status: "success".to_string(),
         results: todos.len(),
         data: todos,
-    })
+    });
+
+    return Ok((StatusCode::OK, Json(json_response)));
 }
 
 // ----------------------------------------------------------------- UPDATE_TODO
 pub async fn update_todo_handler(
-    Path(id): Path<Uuid>,
-    State(db): State<DB>,
+    Path(id): Path<uuid::Uuid>,
+    State(data): State<Arc<AppState>>,
     Json(body): Json<UpdateToDo>,
 ) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
-    let id = id.to_string();
-    let mut vec = db.lock().await;
+    let query = sqlx::query_as!(ToDoModel, "SELECT * FROM todo WHERE id = $1", id)
+        .fetch_one(&data.db)
+        .await;
 
-    if let Some(todo) = vec.iter_mut().find(|todo| todo.id == Some(id.clone())) {
-        let datetime = chrono::Utc::now();
-        let title = body
-            .title
-            .to_owned()
-            .unwrap_or_else(|| todo.title.to_owned());
-        let content = body
-            .content
-            .to_owned()
-            .unwrap_or_else(|| todo.content.to_owned());
-        let complete = body.complete.unwrap_or(todo.complete.unwrap());
-        let payload = ToDo {
-            id: todo.id.to_owned(),
-            title: if !title.is_empty() {
-                title
-            } else {
-                todo.title.to_owned()
-            },
-            content: if !content.is_empty() {
-                content
-            } else {
-                todo.content.to_owned()
-            },
-            complete: Some(complete),
-            createdAt: todo.createdAt,
-            updatedAt: Some(datetime),
-        };
-        *todo = payload;
-
-        let json_response: ToDoSingleResponse = ToDoSingleResponse {
-            status: "success".to_string(),
-            data: todo.clone(),
-        };
-        Ok((StatusCode::OK, Json(json_response)))
-    } else {
-        let error_response = serde_json::json!({
-            "status": "fail",
-            "message": format!("Todo with ID: {} not found", id)
+    if query.is_err() {
+        let error_response = serde_json::json!(GenericResponse {
+            status: "fail".to_string(),
+            message: format!("ToDo with ID: {} not found", id)
         });
+        return Err((StatusCode::NOT_FOUND, Json(error_response)));
+    }
 
-        Err((StatusCode::NOT_FOUND, Json(error_response)))
+    let now = chrono::Utc::now();
+    let todo = query.unwrap();
+
+    let query = sqlx::query_as!(
+        ToDoModel,
+        "UPDATE todo SET title = $1, content = $2, complete = $3, updated_at = $4 WHERE id = $5 RETURNING *",
+        body.title.to_owned().unwrap_or(todo.title),
+        body.content.to_owned().unwrap_or(todo.content),
+        body.complete.unwrap_or(todo.complete.unwrap()),
+        now,
+        id
+    )
+    .fetch_one(&data.db)
+    .await;
+
+    match query {
+        Ok(todo) => {
+            let json_response = serde_json::json!(ToDoSingleResponse {
+                status: "success".to_string(),
+                data: todo
+            });
+
+            return Ok((StatusCode::OK, Json(json_response)));
+        }
+        Err(err) => {
+            return Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(json!(GenericResponse {
+                    status: "error".to_string(),
+                    message: format!("{:?}", err)
+                })),
+            ));
+        }
     }
 }
 
 // ----------------------------------------------------------------- DELETE_TODO
 pub async fn delete_todo_handler(
-    Path(id): Path<Uuid>,
-    State(db): State<DB>,
+    Path(id): Path<uuid::Uuid>,
+    State(data): State<Arc<AppState>>,
 ) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
-    let id = id.to_string();
-    let mut vec = db.lock().await;
+    let query = sqlx::query!("DELETE FROM todo WHERE id = $1", id)
+        .execute(&data.db)
+        .await
+        .unwrap()
+        .rows_affected();
 
-    if let Some(t) = vec.iter().position(|todo| todo.id == Some(id.clone())) {
-        vec.remove(t);
-        return Ok((StatusCode::NO_CONTENT, Json("")));
+    if query == 0 {
+        let error_response = serde_json::json!(GenericResponse {
+            status: "fail".to_string(),
+            message: format!("ToDo with ID: {} not found", id)
+        });
+
+        return Err((StatusCode::NOT_FOUND, Json(error_response)));
     }
-    let error_response = serde_json::json!({
-        "status": "fail",
-        "message": format!("Todo with ID: {} not found", id)
-    });
 
-    return Err((StatusCode::NOT_FOUND, Json(error_response)));
+    return Ok((StatusCode::NO_CONTENT, Json({})));
 }
