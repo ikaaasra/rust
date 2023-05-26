b2fc7b86dac22223b5da21132a9e260c0f9ca79c
Kasra Alizadeh <ikaaasra@gmail.com>
2023-05-26 04:03:47 +0330

    add auth.rs

---
+++ src/handlers/auth.rs
@@ -0,0 +1,164 @@
+use crate::{
+    model::UserModel,
+    schema::{GenericResponse, Signin, Signup, UserSingleResponse, JWT},
+    AppState,
+};
+use argon2::{password_hash::SaltString, Argon2, PasswordHash, PasswordHasher, PasswordVerifier};
+use axum::{
+    extract::State,
+    http::{header, Response, StatusCode},
+    response::IntoResponse,
+    Extension, Json,
+};
+
+use axum_extra::extract::cookie::{Cookie, SameSite};
+use jsonwebtoken::{encode, EncodingKey, Header};
+use rand_core::OsRng;
+use std::sync::Arc;
+
+// ----------------------------------------------------------------- SIGNUP_TODO
+pub async fn signup_handler(
+    State(data): State<Arc<AppState>>,
+    Json(body): Json<Signup>,
+) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
+    let qqq: Option<bool> =
+        sqlx::query_scalar("SELECT EXISTS(SELECT 1 FROM users WHERE mail = $1)")
+            .bind(body.mail.to_owned().to_ascii_lowercase())
+            .fetch_one(&data.db)
+            .await
+            .map_err(|e| {
+                return (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    Json(serde_json::json!(GenericResponse {
+                        status: "fail".to_string(),
+                        message: format!("Database error: {}", e),
+                    })),
+                );
+            })?;
+
+    if let Some(exists) = qqq {
+        if exists {
+            return Err((
+                StatusCode::CONFLICT,
+                Json(serde_json::json!(GenericResponse {
+                    status: "fail".to_string(),
+                    message: "User with that mail already exists".to_string(),
+                })),
+            ));
+        }
+    }
+
+    let salt = SaltString::generate(&mut OsRng);
+    let hashed_password = Argon2::default()
+        .hash_password(body.password.as_bytes(), &salt)
+        .map_err(|err| {
+            let response = serde_json::json!(GenericResponse {
+                status: "fail".to_string(),
+                message: format!("Error while hashing password: {}", err),
+            });
+            (StatusCode::INTERNAL_SERVER_ERROR, Json(response))
+        })
+        .map(|hash| hash.to_string())?;
+
+    let query = sqlx::query_as!(
+        UserModel,
+        "INSERT INTO users (name,mail,password) VALUES ($1, $2, $3) RETURNING *",
+        body.name.to_string(),
+        body.mail.to_string().to_ascii_lowercase(),
+        hashed_password,
+    )
+    .fetch_one(&data.db)
+    .await
+    .map_err(|e| {
+        let response = serde_json::json!(GenericResponse {
+            status: "fail".to_string(),
+            message: format!("Database error: {}", e),
+        });
+        (StatusCode::INTERNAL_SERVER_ERROR, Json(response))
+    })?;
+
+    let response = serde_json::json!(UserSingleResponse {
+        status: "success".to_string(),
+        data: query
+    });
+
+    return Ok((StatusCode::OK, Json(response)));
+}
+
+// ----------------------------------------------------------------- SIGNIN_TODO
+pub async fn signin_handler(
+    State(data): State<Arc<AppState>>,
+    Json(body): Json<Signin>,
+) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
+    let query = sqlx::query_as!(
+        UserModel,
+        "SELECT * FROM users WHERE mail = $1",
+        body.mail.to_ascii_lowercase()
+    )
+    .fetch_optional(&data.db)
+    .await
+    .map_err(|err| {
+        let response = serde_json::json!(GenericResponse {
+            status: "error".to_string(),
+            message: format!("Database error: {}", err),
+        });
+        (StatusCode::INTERNAL_SERVER_ERROR, Json(response))
+    })?
+    .ok_or_else(|| {
+        let response = serde_json::json!(GenericResponse {
+            status: "fail".to_string(),
+            message: "Invalid mail or password".to_string(),
+        });
+        (StatusCode::BAD_REQUEST, Json(response))
+    })?;
+
+    let valid = match PasswordHash::new(&query.password) {
+        Ok(parsed_hash) => Argon2::default()
+            .verify_password(body.password.as_bytes(), &parsed_hash)
+            .map_or(false, |_| true),
+        Err(_) => false,
+    };
+
+    if !valid {
+        return Err((
+            StatusCode::BAD_REQUEST,
+            Json(serde_json::json!(GenericResponse {
+                status: "fail".to_string(),
+                message: "Invalid mail or password".to_string()
+            })),
+        ));
+    }
+
+    let now = chrono::Utc::now();
+    let iat = now.timestamp() as usize;
+    let exp = (now + chrono::Duration::minutes(60)).timestamp() as usize;
+    let claims: JWT = JWT {
+        sub: query.id.to_string(),
+        exp,
+        iat,
+    };
+
+    let token = encode(
+        &Header::default(),
+        &claims,
+        &EncodingKey::from_secret(data.env.jwt_secret.as_ref()),
+    )
+    .unwrap();
+
+    let cookie = Cookie::build("token", token.to_owned())
+        .path("/")
+        .max_age(time::Duration::hours(1))
+        .same_site(SameSite::Lax)
+        .http_only(true)
+        .finish();
+
+    let mut response =
+        Response::new(serde_json::json!({"status": "success", "token": token}).to_string());
+    response
+        .headers_mut()
+        .insert(header::SET_COOKIE, cookie.to_string().parse().unwrap());
+
+    // Ok(response)
+
+    return Ok((StatusCode::OK, Json({})));
+}
