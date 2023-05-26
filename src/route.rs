813f569698a48304fce0c29a2d3bca2051ee8186
Kasra Alizadeh <ikaaasra@gmail.com>
2023-05-26 04:02:54 +0330

    update route.rs

--- src/route.rs
+++ src/route.rs
@@ -1,11 +1,19 @@
 use crate::{
-    handler::{
-        create_todo_handler, delete_todo_handler, get_todo_handler, get_todos_handler,
-        health_handler, update_todo_handler,
+    handlers::{
+        auth::{signin_handler, signup_handler},
+        health::health_handler,
+        todo::{
+            create_todo_handler, delete_todo_handler, get_todo_handler, get_todos_handler,
+            update_todo_handler,
+        },
     },
+    // jwt_auth::auth,
     AppState,
 };
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, post},
+    Router,
+};
 use std::sync::Arc;
 
 pub fn router(app_state: Arc<AppState>) -> Router {
@@ -21,5 +29,17 @@ pub fn router(app_state: Arc<AppState>) -> Router {
                 .delete(delete_todo_handler)
                 .patch(update_todo_handler),
         )
+        .route("/auth/signin", post(signin_handler))
+        .route("/auth/signup", post(signup_handler))
+        // .route(
+        //     "/api/auth/logout",
+        //     get(logout_handler)
+        //         .route_layer(middleware::from_fn_with_state(app_state.clone(), auth)),
+        // )
+        // .route(
+        //     "/api/users/me",
+        //     get(get_userme_handler)
+        //         .route_layer(middleware::from_fn_with_state(app_state.clone(), auth)),
+        // )
         .with_state(app_state);
 }
